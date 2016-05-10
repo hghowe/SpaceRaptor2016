1d5d801029a963fb290a1a73267d4ed0d489c00a
Harlan Howe <hghowe@gmail.com>
2016-05-09 22:00:40 -0500

    Added constants for identifying message types and object types

--- HH-StarRaptor/src/Constants.java
+++ HH-StarRaptor/src/Constants.java
@@ -2,4 +2,12 @@
 public interface Constants {
 	public final String MJR_DIVIDER = "<>";
 	public final String MNR_DIVIDER = "\t";
+	
+	public final int PREFIX_NEW_OBJECT = 0;
+	public final int PREFIX_UPDATE_OBJECT = 1;
+	public final int PREFIX_REMOVE_OBJECT = 2;
+	
+	public final int TYPE_RAPTOR = 0;
+	public final int TYPE_BULLET = 1;
+	
 }
