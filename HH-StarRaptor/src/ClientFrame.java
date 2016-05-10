c8b8abbad55945dd251e32620877331afec7ce14
Harlan Howe <hghowe@gmail.com>
2016-05-09 21:11:59 -0500

    origin

---
+++ HH-StarRaptor/src/ClientFrame.java
@@ -0,0 +1,26 @@
+import java.awt.Color;
+import java.awt.GraphicsConfiguration;
+import java.awt.GridLayout;
+import java.awt.HeadlessException;
+
+import javax.swing.JFrame;
+
+public class ClientFrame extends JFrame {
+
+	private GamePanel myPanel;
+	
+	public ClientFrame() 
+	{
+		super("StarRaptor");
+		setSize(800,800);
+		setBackground(Color.BLACK);
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		getContentPane().setLayout(new GridLayout(1,1));
+		myPanel = new GamePanel();
+		getContentPane().add(myPanel);
+		setVisible(true);
+	}
+
+
+}
