9718c9fe410578b58486108a5a130be2a2611c7c
Harlan Howe <hghowe@gmail.com>
2016-05-09 23:20:19 -0500

    Created TURN_RATE and THRUST_POWER constants for controlling physics of the Raptors.

--- HH-StarRaptor/src/Constants.java
+++ HH-StarRaptor/src/Constants.java
@@ -11,4 +11,7 @@ public interface Constants {
 	public final int TYPE_BULLET = 1;
 	
 	public final String IP_ADDRESS = "172.16.220.122";
+	
+	public final double TURN_RATE = 3.14159;
+	public final double THRUST_POWER = 100;
 }
