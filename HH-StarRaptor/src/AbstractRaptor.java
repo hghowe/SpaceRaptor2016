e53c691bb34bc1b7b74cb123866fcec8f5fc7614
Harlan Howe <hghowe@gmail.com>
2016-05-09 21:01:09 -0500

    Updated AbstractRaptor to implement Transmittable interface

--- HH-StarRaptor/src/AbstractRaptor.java
+++ HH-StarRaptor/src/AbstractRaptor.java
@@ -1,30 +1,46 @@
 
-public class AbstractRaptor {
+public class AbstractRaptor implements Transmittable {
 
+	private int id;
+	private String initials;
 	private double xPos, yPos, angle;
 	
 	public AbstractRaptor() {
+		id = -1;
+		initials = "AAA";
 		xPos = 0;
 		yPos = 0;
 		angle = 0;
 	}
 
-	public String setFromString(String s)
-	{
-		String[] parts = s.split("\t");
-		xPos = Double.parseDouble(parts[0]);
-		yPos = Double.parseDouble(parts[1]);
-		angle = Double.parseDouble(parts[2]);
-		String remains = "";
-		for (int i = 3; i<parts.length; i++)
-		{
-			remains+= parts[i];
-			if (i<parts.length-1)
-				remains+="\t";
-		}
-		return remains;
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
 	}
-	
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the initials
+	 */
+	public String getInitials() {
+		return initials;
+	}
+
+	/**
+	 * @param initials the initials to set
+	 */
+	public void setInitials(String initials) {
+		this.initials = initials;
+	}
+
 	/**
 	 * @return the xPos
 	 */
@@ -67,14 +83,64 @@ public class AbstractRaptor {
 	public void setAngle(double angle) {
 		this.angle = angle;
 		while (this.angle<0)
-			this.angle+=2*Math.PI;
+			this.angle += 2*Math.PI;
 		while (this.angle>=2*Math.PI)
-			this.angle-=2*Math.PI;
+			this.angle -= 2*Math.PI;
 	}
 
-	public String toString()
+	/** (fulfilling Transmittable)
+	 * create a string describing just the information needed to <i>update</i> this object
+	 * @return description
+	 */
+	public String shortDescription()
 	{
-		return xPos+"\t"+yPos+"\t"+angle;
+		return  id+Constants.MNR_DIVIDER+
+				xPos+Constants.MNR_DIVIDER+
+				yPos+Constants.MNR_DIVIDER+
+				angle;
 	}
+	
+	/** (fulfilling Transmittable)
+	 * create a string describing the information needed to <i>create</i> this object
+	 * @return description
+	 */
+	public String longDescription()
+	{
+		return  id+Constants.MNR_DIVIDER+
+				initials+Constants.MNR_DIVIDER+
+				xPos+Constants.MNR_DIVIDER+
+				yPos+Constants.MNR_DIVIDER+
+				angle;
+	}
+	
+	/** (fulfilling Transmittable)
+	 * set the fields for this class, based on a description string of the form 
+	 * made by longDescription().
+	 * @param longDesc
+	 */
+	public void buildFromDescription(String longDesc)
+	{
+		String [] parts = longDesc.split(Constants.MNR_DIVIDER);
+		setId(Integer.parseInt(parts[0]));
+		setInitials(parts[1]);
+		setxPos(Double.parseDouble(parts[2]));
+		setyPos(Double.parseDouble(parts[3]));
+		setAngle(Double.parseDouble(parts[4]));
+	}
+	
+	/** (fulfilling Transmittable)
+	 * update the fields for this class, based on a description string of the form 
+	 * made by shortDescription().
+	 * @param shortDesc
+	 */
+	public void updateFromDescription(String shortDesc)
+	{
+		String [] parts = shortDesc.split(Constants.MNR_DIVIDER);
+		setId(Integer.parseInt(parts[0]));
+		setxPos(Double.parseDouble(parts[1]));
+		setyPos(Double.parseDouble(parts[2]));
+		setAngle(Double.parseDouble(parts[3]));
+	}
+	
 	
 }
