ac83dc660332530f35be5de0ab96137ddc9fa186
Harlan Howe <hghowe@gmail.com>
2016-05-09 23:19:41 -0500

    Added simple physics model.

--- HH-StarRaptor/src/ServerRaptor.java
+++ HH-StarRaptor/src/ServerRaptor.java
@@ -1,8 +1,32 @@
 
 public class ServerRaptor extends AbstractRaptor {
 
+	private double vx, vy;
+	private boolean isTurningLeft, isTurningRight, isThrusting;
+	
+	
 	public ServerRaptor() {
-		// TODO Auto-generated constructor stub
+		super();
+		vx = 0;
+		vy = 0;
+		isTurningLeft = false;
+		isTurningRight = false;
+		isThrusting = false;
 	}
-
+	
+	public void step(double deltaT)
+	{
+		if (isTurningLeft)
+			setAngle(getAngle()-Constants.TURN_RATE*deltaT);
+		if (isTurningRight)
+			setAngle(getAngle()+Constants.TURN_RATE*deltaT);
+		if (isThrusting)
+		{
+			vx += Constants.THRUST_POWER*Math.cos(getAngle())*deltaT;
+			vy += Constants.THRUST_POWER*Math.sin(getAngle())*deltaT;
+		}
+		
+		setxPos(getxPos()+vx*deltaT);
+		setyPos(getyPos()+vy*deltaT);
+	}
 }
