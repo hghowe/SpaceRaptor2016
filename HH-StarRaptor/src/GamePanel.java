6dfe7b315f54d59ea5d08e0f7c5099aa8186924b
Harlan Howe <hghowe@gmail.com>
2016-05-11 20:23:14 -0500

    Manages add/update/remove from objectsOnScreen in a way that won't cause ConcurrentModification errors from multithreading. It now puts the changes from the StarRaptorClient thread into holding lists, and only changes the objectsOnScreen immediately before looping through them to draw.

--- HH-StarRaptor/src/GamePanel.java
+++ HH-StarRaptor/src/GamePanel.java
@@ -2,6 +2,7 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,6 +14,9 @@ public class GamePanel extends JPanel implements KeyListener{
 	private Map<Integer,Transmittable> objectsOnScreen;
 	private boolean leftPressed, rightPressed, thrustPressed, firePressed;
 	private StarRaptorClient theClient;
+	private ArrayList<String> objectsToUpdate;
+	private ArrayList<Transmittable> objectsToAdd;
+	private ArrayList<Integer> objectsToRemove;
 	
 	public GamePanel() {
 		super();
@@ -21,15 +25,58 @@ public class GamePanel extends JPanel implements KeyListener{
 		rightPressed = false;
 		thrustPressed = false;
 		firePressed = false;
-		theClient = new StarRaptorClient(objectsOnScreen, requestInitials(), this);
+		theClient = new StarRaptorClient(requestInitials(), this);
 		setBackground(Color.BLACK);
+		objectsToUpdate = new ArrayList<String>();
+		objectsToAdd = new ArrayList<Transmittable>();
+		objectsToRemove = new ArrayList<Integer>();
 	}
 
+	public void addObject(Transmittable obj)
+	{
+		objectsToAdd.add(obj);
+	}
+	
+	public void updateObject(String s)
+	{
+		objectsToUpdate.add(s);
+	}
+	
+	public void removeObject(int i)
+	{
+		objectsToRemove.add(i);
+	}
+	/**
+	 * takes the changes queued up in objectsToAdd/Update/Remove and makes these changes
+	 * to objectsOnScreen.
+	 */
+	public void updateObjectsOnScreen()
+	{
+		while (!objectsToAdd.isEmpty())
+		{
+			Transmittable obj = objectsToAdd.remove(0);
+			int id = Integer.parseInt(obj.longDescription().split(Constants.MNR_DIVIDER)[0]);
+			objectsOnScreen.put(id, obj);
+		}
+		while (!objectsToUpdate.isEmpty())
+		{
+			String shortDesc = objectsToUpdate.remove(0);
+			int id = Integer.parseInt(shortDesc.split(Constants.MNR_DIVIDER)[0]);
+			objectsOnScreen.get(id).updateFromDescription(shortDesc);
+		}
+		while (!objectsToRemove.isEmpty())
+		{
+			objectsOnScreen.remove(objectsToRemove.remove(0));
+		}
+	}
+	
 	/**
 	 * draws the screen. This will automatically be called shortly after repaint(), on a regular cycle.
+	 * first updates the contents of objectsOnScreen.
 	 */
 	public void paintComponent(Graphics g)
 	{
+		updateObjectsOnScreen();
 		super.paintComponent(g);
 		for (Integer key:objectsOnScreen.keySet())
 		{
